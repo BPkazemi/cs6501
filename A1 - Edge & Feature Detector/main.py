e34a239e98223be98cc84cecc40b0a3623fcc8e4
Babak Pourkazemi <bp5xj@virginia.edu>
2015-02-08 21:06:05 -0500

    SIFT v1.0

--- A1 - Edge & Feature Detector/main.py
+++ A1 - Edge & Feature Detector/main.py
@@ -1,4 +1,5 @@
 import pylab
+import pdb
 import numpy
 import os, os.path
 import skimage
@@ -9,7 +10,7 @@ from skimage import io, color
 from scipy import ndimage, signal, mgrid, misc, linalg
 
 ## GLOBAL VARS ##
-sigma = 1.6
+sigma = 1.0
 
 ## Utils ##
 def gauss_partials( sigma ):
@@ -49,12 +50,16 @@ def showim( i ):
     pylab.imshow(i, cmap="gray")
     pylab.show()
 
-    ''' "Multiple images" 
-        fig = pylab.figure()
-        a = fig.add_subplot(1, nScales, (i+1))
-        pylab.imshow(scaledImages[i], cmap="gray")
-        pylab.show()
-    '''
+def showkpts( kpts, I, maxS ):
+    ## Spatial data-structure would speed this up (kd-tree, BSP tree) ##
+    pylab.imshow( I, cmap="gray" )
+    for x in range(len(keypoints)):
+        for y in range(len(keypoints[x])):
+            for s in range(len(keypoints[x][y])):
+                if keypoints[x][y][s] != 0:
+                    pylab.plot( y, x, marker='o', ms=((3.*s/maxS)**2), c='y', mfc='none' )
+    pylab.show()
+
 def showims( imgs ):
     fig = pylab.figure()
     for i in range(len(imgs)):
@@ -227,27 +232,27 @@ def find_edge_chains( I, x, y, t_edges, direction, t_low ):
         if direction == 0:
             # Check East-West
             if not at_left and not t_edges[x][y-1]:
-                find_edge_chains( I, t_edges, direction, x, y-1, t_low )
+                find_edge_chains( I, x, y-1, t_edges, direction,t_low )
             if not at_right and not t_edges[x][y+1]:
-                find_edge_chains( I, t_edges, direction, x, y+1, t_low )
+                find_edge_chains( I, x, y+1, t_edges, direction, t_low )
         elif direction == 45:
             # Check Northeast-Southwest
             if not at_right and not at_top and not t_edges[x-1][y+1]:
-                find_edge_chains( I, t_edges, direction, x-1, y+1, t_low )
+                find_edge_chains( I, x-1, y+1, t_edges, direction, t_low )
             if not at_left and not at_bottom and not t_edges[x+1][y-1]:
-                find_edge_chains( I, t_edges, direction, x+1, y-1, t_low )
+                find_edge_chains( I, x+1, y-1, t_edges, direction, t_low )
         elif direction == 90:
             # Check North-South
             if not at_top and not t_edges[x-1][y]:
-                find_edge_chains( I, t_edges, direction, x-1, y, t_low )
+                find_edge_chains( I, x-1, y, t_edges, direction, t_low )
             if not at_bottom and not t_edges[x+1][y]:
-                find_edge_chains( I, t_edges, direction, x+1, y, t_low )
+                find_edge_chains( I, x+1, y, t_edges, direction, t_low )
         elif direction == 135:
             # Check Northwest-Southeast
             if not at_left and not at_top and not t_edges[x-1][y-1]:
-                find_edge_chains( I, t_edges, direction, x-1, y-1, t_low )
+                find_edge_chains( I, x-1, y-1, t_edges, direction, t_low )
             if not at_right and not at_bottom and not t_edges[x+1][y+1]:
-                find_edge_chains( I, t_edges, direction, x+1, y+1, t_low )
+                find_edge_chains( I, x+1, y+1, t_edges, direction, t_low )
 
 ## Util
 def gradient_components( I ):
@@ -366,14 +371,15 @@ def downsample( I, factor ):
 
 def findScaleSpaceExtrema( I, nOctaves, nScales, delSigma, k ):
     ## TODO: Do values of k depend on nScales? Does nScales depend on the number of intervals we seek?
+    ## TODO: Double size of input for first level of pyramid. Use linear extrema_dir. Pre-smooth with sigma=0.5
     print "Calculating keypoints..."
 
-    if nScales < 4:
-        raise Exception( "Too few scales. Must be >= 4.")
-
+    ## TODO: Create s+3 images to cover the whole domain. Necessary?
+    nScales = nScales + 3
     original = I
     octaves = []
     DoGs = {}
+    keypoints = {}
     nKeypoints = 0
     ## 0. For each octave...
     for oc in range( nOctaves ):
@@ -393,8 +399,10 @@ def findScaleSpaceExtrema( I, nOctaves, nScales, delSigma, k ):
             DoGs[oc].append(L2-L1)
 
         ## 3. Find local extrema among DoGs.
-        # keypoints = numpy.zeros( scaledImages[0].shape, dtype=('f4, i4') )
-        keypoints = numpy.zeros( (scaledImages[0].shape[0], scaledImages[0].shape[1], nScales) )
+        keypoints[oc] = numpy.zeros( 
+            (nScales,
+             scaledImages[0].shape[0], 
+             scaledImages[0].shape[1]))
         nTriples = len( DoGs[0] )-2
         curDoGs = DoGs[oc]
         maxS = 0
@@ -407,34 +415,167 @@ def findScaleSpaceExtrema( I, nOctaves, nScales, delSigma, k ):
                 for y in range(len(d1[x])):
                     local_min, local_max = checkNeighbors3( [d0, d1, d2], (x, y), m=3 )
                     if local_min or local_max:
-                        keypoints[x][y][i+1] = d1[x][y]
+                        keypoints[oc][i+1][x][y] = d1[x][y]
                         nKeypoints += 1
                         maxS = max( maxS, i+1 )
 
         sampleIndex = int(round(math.log(2) / math.log(k)))  # k^x = 2 -> Index where blur = 2*sigma
         I = downsample( scaledImages[sampleIndex], 2 )
 
-        ## Spatial data-structure would speed this up (kd-tree, BSP tree) ##
-        pylab.imshow( original, cmap="gray" )
-        for x in range(len(keypoints)):
-            for y in range(len(keypoints[x])):
-                for s in range(len(keypoints[x][y])):
-                    if keypoints[x][y][s] != 0:
-                        pylab.plot( y, x, marker='o', ms=((3.*s/maxS)**2), c='y', alpha=0.4 )
-        pylab.show()
+        # showkpts( keypoints[oc], original, maxS )
 
-
+    I = original
     print str(nKeypoints) + " keypoints found"
-    return keypoints
+    return keypoints, DoGs
 
-def localizeKeypoints():
+def taylor_series( (s, x, y), grad1, grad2 ):
+    # Derivatives of Taylor Series approximated with finite differences
+    # pdb.set_trace()
+    dSigma = grad1[0][s][x][y]
+    dX = grad1[1][s][x][y]
+    dY = grad1[2][s][x][y]
+
+    dSigmaSigma = grad2[0][0][s][x][y]
+    dXX = grad2[1][1][s][x][y]
+    dYY = grad2[2][2][s][x][y]
+
+    dSigmaX = grad2[0][1][s][x][y]
+    dSigmaY = grad2[0][2][s][x][y]
+    dYX = grad2[2][1][s][x][y]
+
+    d1D = numpy.zeros( ( 3, 1 ) )
+    d1D[0][0] = dSigma
+    d1D[1][0] = dX
+    d1D[2][0] = dY
+    d2D = numpy.array(( 
+        [dSigmaSigma, dSigmaY, dSigmaX], 
+        [dSigmaY, dYY, dYX], 
+        [dSigmaX, dYX, dXX] 
+    ))
+
+    # Must be invertible 
+    # -- This function looks pretty good --
+    if abs(linalg.det(d2D)) > 1e-10:
+        extrema_dir = -linalg.inv(d2D).dot(d1D)  # indices: sigma, y, x
+        return numpy.round(extrema_dir).astype(int)
+    else:
+        return numpy.zeros((3, 1))
+
+def find_extrema( (s, x, y), i, grad1, grad2 ):
+    # pdb.set_trace()
+    extrema_dir = taylor_series((s,x,y), grad1, grad2)  ## Returned same thing twice...Looked like a fluke
+
+    if i >= 4:
+        # Prevent thrashing
+        return extrema_dir
+
+    ## TODO: Is absolute value necessary?
+    ## -- This function looks pretty good --
+    if (
+        len([num[0] for num in extrema_dir if abs(num[0]) > 0.5]) > 0
+    ):
+        # Maximum is far enough away to repeat process
+        newX, newY, newS = x, y, s
+        if abs(extrema_dir[0][0]) > 0.5:
+            newS += extrema_dir[0][0]
+        if abs(extrema_dir[1][0]) > 0.5:
+            newY += extrema_dir[1][0]
+        if abs(extrema_dir[2][0]) > 0.5:
+            newX += extrema_dir[2][0]
+
+        newS = min(len(grad1[0])-1, max(0, newS))
+        newX = min(len(grad1[0][0])-1, max(0, newX))
+        newY = min(len(grad1[0][0][0])-1, max(0, newY))
+
+        # Repeat with better location
+        return find_extrema( (newS, newX, newY), (i+1), grad1, grad2 )
+    else:
+        return extrema_dir
+
+def filter_low_contrast( grad, DoGs, extrema_locs, curOctave):
+    # TODO: Normalize intensities to [0,1]
+    numTossed = 0
+    filtered_keypoints = []
+    for i in range(len(extrema_locs[curOctave])):
+        # For each (accurate) keypoint, calculate D(x) - 0.5*dD(X)
+        (s, x, y) = extrema_locs[curOctave][i]
+        Dx = DoGs[s][x][y] - 0.5*grad[curOctave][s][x][y]
+        if Dx >= 0.03:
+            filtered_keypoints.append( (s,x,y) )
+        else:
+            numTossed += 1
+
+    print str(numTossed) + " low-contrast keypoints removed"
+    return filtered_keypoints
+
+def filter_hessian( grad2, filtered_keypoints ):
+    ## Compute Hessian of D, use as threshold
+    numTossed = 0
+    hessian_threshold = []
+    for i in range(len(filtered_keypoints)):
+        (s, x, y) = filtered_keypoints[i]
+        dXX = grad2[1][1][s][x][y]
+        dYY = grad2[2][2][s][x][y]
+        dXY = grad2[1][2][s][x][y]
+
+        H = numpy.array(( [dXX, dXY], [dXY, dYY] ))
+        trace = H.trace()
+        det = linalg.det( H )
+
+        r = 10.
+
+        lhs = trace**2. / det
+        rhs = (r+1.)**2 / r
+        if (lhs < rhs):
+            hessian_threshold.append( (s, x, y) )
+        else:
+            numTossed += 1
+
+    print str(numTossed) + " keypoints along edges removed"
+    return hessian_threshold
+
+def localizeKeypoints( keypoints, allDoGs ):
     print "Localizing keypoints..."
-    ## 1. Filter out low-contrast points
 
-    ## 2. Discard keypoints along edges
-    ## 2a. Compute Hessian of D
+    # For each octave, localize keypoints
+    improved_keypoints = {}
+    filtered_keypoints = {}
+    for oc in range(len(allDoGs)):
+        DoGs = allDoGs[oc]
+        improved_keypoints[oc] = []  ## Holds the x, y, s locations of the new keypoints
+        filtered_keypoints[oc] = []
+        
+        ## 1. Filter out low-contrast points
+        grad1 = numpy.gradient( DoGs )
+        grad2 = numpy.gradient( grad1 )
 
-    ## 2b. Calculate trace, determinant, and r
+        keypoints = keypoints[oc]
+        for s in range(len(keypoints)):
+            for x in range(len(keypoints[s])):
+                for y in range(len(keypoints[s][x])):
+                    if keypoints[s][x][y] != 0:
+                        # Approximate Taylor Series expansion with finite differences
+                        extrema_dir = find_extrema((s,x,y), 0, grad1, grad2)
+
+                        # extrema_dir indices: s, y, x
+                        newS = s + extrema_dir[0][0]
+                        newY = y + extrema_dir[1][0]
+                        newX = x + extrema_dir[2][0]
+
+                        # grad1 indices: s, x, y
+                        newS = int(min(len(grad1[0])-1, max(0, newS)))
+                        newX = int(min(len(grad1[0][0])-1, max(0, newX)))
+                        newY = int(min(len(grad1[0][0][0])-1, max(0, newY)))
+
+                        improved_keypoints[oc].append((newS, newX, newY))
+
+        filtered_keypoints[oc] = filter_low_contrast( grad1, DoGs, improved_keypoints, curOctave=oc )
+
+        ## 2. Discard keypoints along edges
+        post_hfilter = filter_hessian( grad2, filtered_keypoints[oc] )
+        filtered_keypoints[oc] = post_hfilter 
+
+    return filtered_keypoints 
 
 if __name__ == "__main__":
     print "~~~ Edge, Line, & Feature Detector ~~~"
@@ -484,10 +625,19 @@ if __name__ == "__main__":
         start = time.clock()
 
         ## 1. Find scale-space extrema
-        findScaleSpaceExtrema( I, nOctaves=1, nScales=6, delSigma=1.6, k=2.**(1./3.) )
+        # k = 2^(1/s)
+        # TODO: Multiple octaves
+        kpts, dogs = findScaleSpaceExtrema( I, nOctaves=1, nScales=1, delSigma=1.6, k=2.**(1./3.) )
 
         ## 2. Localize keypoints
-        localizeKeypoints()
+        kpts2 = localizeKeypoints( kpts, dogs )
+
+        pylab.imshow( I, cmap="gray" )
+        for oc in range(len(kpts2)):
+            for i in range(len(kpts2[oc])):
+                (s, x, y) = kpts2[0][i]
+                pylab.plot( y, x, marker='o', ms=5, mec='y', mfc='none' )
+            pylab.show()
 
         end = time.clock()
         print "SIFT feature detection completed in " + str(end - start) + "s"
